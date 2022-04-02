84f264f0a821e4c1139aaf96140da6446ea3ae47
Wasab <whitenoise09@free.fr>
2022-04-03 00:04:34 +0200

    Resolved a bug on chromium with res.statusText being empty on gh-pages. Switched to res.ok

--- src/languages.mjs
+++ src/languages.mjs
@@ -4,7 +4,7 @@ import html from "./html.mjs";
 export async function fetchLangDatas(lang) {
     const res = await fetch(`./contents/${lang}.json`);
     
-    if(res.statusText != 'OK') throw new Error(`No content for ${lang} language yet.`);
+    if(!res.ok) throw new Error(`No content for ${lang} language yet.`);
     
     const json = await res.json();
 
